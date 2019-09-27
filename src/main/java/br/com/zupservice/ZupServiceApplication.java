430052f66c66b3dac73c7baf9bb76768df8d6574
Jose Geraldo Henrique silva <henriquejgs@gmail.com>
2019-09-27 00:47:00 -0300

    Application Start

---
+++ src/main/java/br/com/zupservice/ZupServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZupServiceApplication.class, args);
+    }
+
+}
